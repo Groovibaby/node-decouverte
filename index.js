d0fd6cb1dfaf8cdcffa1155515f2f20e16573cc9
Thomas <thomas.lachieze@gmail.com>
2020-03-25 15:44:16 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+ 
+console.log(chalk.green('Guillaume'));
+console.log(chalk.blue('Johanna'));
+console.log(chalk.yellow('Volcan'));
+console.log(chalk.red('Enzo n\'est pas null'));
